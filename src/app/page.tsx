99f1e310fd365ed83bde43270f6db2ae97181d1d
orangdong <dev.naftaliaviel@gmail.com>
2025-01-05 15:21:21 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // import Image from "next/image";
 
 import DataTab from "@/components/DataTab";
 
 async function fetchFromInternet() {
-  const response = await fetch("https://api.vercel.app/blog");
-  const data = await response.json();
-  return data.map((item: { title: any }) => item.title);
+  try {
+    const response = await fetch("https://api.vercel.app/blog");
+    const data = await response.json();
+    return data.map((item: { title: any }) => item.title);
+  } catch (error) {
+    return [];
+  }
 }
 
-// async function fetchFromUbuntu() {
-//   const response = await fetch("https://api.vercel.app/ubuntu");
-//   const data = await response.json();
-//   return data.map((item: any) => item.title);
-// }
+async function fetchFromUbuntu() {
+  try {
+    const url = process.env.NEXT_PUBLIC_UBUNTU_HOST ?? "";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.map((item: any) => `${item.name} - ${item.email}`);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function fetchWindows() {
+  try {
+    const url = process.env.NEXT_PUBLIC_WINDOWS_HOST ?? "";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.map((item: any) => `${item.name} - ${item.email}`);
+  } catch (error) {
+    return [];
+  }
+}
 
 export default async function Home() {
   const internetData: string[] = await fetchFromInternet();
-  const ubuntuData: string[] = [];
-  const windowsData: string[] = [];
+  const ubuntuData: string[] = await fetchFromUbuntu();
+  const windowsData: string[] = await fetchWindows();
 
   return (
     <div className="ms-5 mt-5">
